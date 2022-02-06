fbc4d231c3c996c87776ebbb29e28ade6969799a
culpritcr <47873209+culpritcr@users.noreply.github.com>
2022-02-06 00:00:19 -0600

    Update Halion.lua
    Fix Twilight Cutter timer.

--- DBM-ChamberOfAspects/Ruby/Halion.lua
+++ DBM-ChamberOfAspects/Ruby/Halion.lua
@@ -46,7 +46,7 @@ local timerMeteorCD					= mod:NewNextTimer(40, 74648, nil, nil, nil, 3)--Target 
 local timerMeteorCast				= mod:NewCastTimer(7, 74648)--7-8 seconds from boss yell the meteor impacts.
 --local timerTwilightCutterCast		= mod:NewCastTimer(5, 74769)
 local timerTwilightCutter			= mod:NewBuffActiveTimer(8, 74769, nil, nil, nil, 6)
-local timerTwilightCutterCD			= mod:NewNextTimer(15, 74769, nil, nil, nil, 6)
+local timerTwilightCutterCD			= mod:NewNextTimer(30, 74769, nil, nil, nil, 6)
 local timerShadowBreathCD			= mod:NewCDTimer(16, 74806, nil, "Tank|Healer", nil, 5)--Edited. Same as debuff timers, same CD, can be merged into 1.
 local timerFieryBreathCD			= mod:NewCDTimer(16, 74525, nil, "Tank|Healer", nil, 5)--But unique icons are nice pertaining to phase you're in ;)
 
@@ -232,14 +232,14 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 			self:SendSync("Meteor")
 		end
 	elseif msg == L.twilightcutter or msg:find(L.twilightcutter) then -- Edited (specific for Warmane since CHAT_MSG_RAID_BOSS_EMOTE fires twice: at 5s and at cutter)
-			specWarnTwilightCutter:Schedule(1)
-			specWarnTwilightCutter:ScheduleVoice(1, "farfromline")
+			specWarnTwilightCutter:Show()
+			specWarnTwilightCutter:Play("farfromline")
 		if not self.Options.AnnounceAlternatePhase then
-			--timerTwilightCutterCD:Cancel()
-			warningTwilightCutter:Show()
+			timerTwilightCutterCD:Cancel()
+			--warningTwilightCutter:Show()
 			--timerTwilightCutterCast:Start()
-			timerTwilightCutter:Schedule(1)--Delay it since it happens 5 seconds after the emote
-			timerTwilightCutterCD:Schedule(15)
+			timerTwilightCutter:Start()--Delay it since it happens 5 seconds after the emote
+			timerTwilightCutterCD:Start()
 		end
 		if self:LatencyCheck() then
 			self:SendSync("TwilightCutter")
@@ -250,11 +250,11 @@ end
 function mod:OnSync(msg, target)
 	if msg == "TwilightCutter" then
 		if self.Options.AnnounceAlternatePhase and self:AntiSpam(7, msg) then -- Edited to circumvent Warmane double cutter boss emote
-			--timerTwilightCutterCD:Cancel()
-			warningTwilightCutter:Show()
+			timerTwilightCutterCD:Cancel()
+			--warningTwilightCutter:Show()
 			--timerTwilightCutterCast:Start()
-			timerTwilightCutter:Schedule(1)--Delay it since it happens 5 seconds after the emote
-			timerTwilightCutterCD:Schedule(15)
+			timerTwilightCutter:Start()--Delay it since it happens 5 seconds after the emote
+			timerTwilightCutterCD:Start()
 		end
 	elseif msg == "Meteor" then
 		if self.Options.AnnounceAlternatePhase then
@@ -295,9 +295,9 @@ function mod:OnSync(msg, target)
 		timerShadowBreathCD:Start(18) -- Edited.
 		timerShadowConsumptionCD:Start(22)--Edited. not exact, 15 seconds from tank aggro, but easier to add 5 seconds to it as a estimate timer than trying to detect this
 		if self:IsHeroic() then --These i'm not sure if they start regardless of drake aggro, or if it should be moved too.
-			timerTwilightCutterCD:Start(36)
+			timerTwilightCutterCD:Start(40)
 		else
-			timerTwilightCutterCD:Start(36)
+			timerTwilightCutterCD:Start(40)
 		end
 	elseif msg == "Phase3" and self.vb.phase < 3 then
 		self:SetStage(3)
